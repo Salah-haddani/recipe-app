e3e67ad4e3c567febd830975b2a19b3163771439
Salah Haddani <salahabdelwahabe@example.com>
2026-01-26 18:04:03 +0100

    create auth.service.ts

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+    if (!user) throw new UnauthorizedException('invalid Credentials');
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) throw new UnauthorizedException('invalid Credentials');
+    return user;
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.validateUser(email, password);
+    const payload = { sub: user._id, email: user.email, role: user.role };
+    return { access_token: this.jwtService.sign(payload) };
+  }
+
+  async register(email: string, password: string) {
+    const existingUser = await this.usersService.findByEmail(email);
+    if (existingUser) throw new UnauthorizedException('Email already exists');
+    const user = await this.usersService.create(email, password);
+    const payload = { sub: user._id, email: user.email, role: user.role };
+    return { access_token: this.jwtService.sign(payload) };
+  }
+}
