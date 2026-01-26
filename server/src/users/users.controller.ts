cce6e1796320f65f0f89251cb4df0b6662612bf6
Salah Haddani <salahabdelwahabe@example.com>
2026-01-26 16:35:07 +0100

    create users.controller.ts

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string) {
+    return this.usersService.findById(id);
+  }
 }
