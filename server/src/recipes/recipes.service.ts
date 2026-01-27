3994c3b8e92fd65e923682b5bbb7ec0ec02b95db
Salah Haddani <salahabdelwahabe@example.com>
2026-01-27 22:19:47 +0100

    add recipe service

--- server/src/recipes/recipes.service.ts
+++ server/src/recipes/recipes.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class RecipesService {}
+export class RecipesService {
+  constructor(@InjectModel('Recipe') private recipeModel: Model<any>) {}
+
+  async findAll() {
+    return this.recipeModel.find().populate('owner', 'email');
+  }
+  async findOne(id: string) {
+    const recipe = await this.recipeModel.findById(id);
+    if (!recipe) throw new NotFoundException('recipe not found');
+    return recipe;
+  }
+  async create(data: any, userId: string) {
+    const newRecipe = new this.recipeModel({ ...data, owner: userId });
+    return newRecipe.save();
+  }
+  async update(id: string, data: any) {
+    return this.recipeModel.findByIdAndUpdate(id, data, { new: true });
+  }
+  async delete(id: string) {
+    return this.recipeModel.findByIdAndDelete(id);
+  }
+}
