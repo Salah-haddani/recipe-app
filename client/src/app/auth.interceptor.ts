ee297a32275325635b3ee27070f1db2973296d25
Salah Haddani <salahabdelwahabe@example.com>
2026-01-27 22:58:37 +0100

    add auth interceptor

---
+++ client/src/app/auth.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('access_token');
+
+  // add the Bearer token to requests
+  if (token) {
+    const cloned = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(cloned);
+  }
+
+  return next(req);
+};
