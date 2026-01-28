c0886dee951383c16e445753ee34dc7769ef97e7
Salah Haddani <salahabdelwahabe@example.com>
2026-01-28 18:37:06 +0100

    add recipe details component to routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { RecipeDetailsComponent } from './pages/recipe-details/recipe-details.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'recipe/:id', component: RecipeDetailsComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
