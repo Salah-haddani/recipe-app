6a38be9fb5b0441de16ce05427d9dda8956a4d14
Salah Haddani <salahabdelwahabe@example.com>
2026-01-28 15:37:57 +0100

    edit imports in app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MatButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
