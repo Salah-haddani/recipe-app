f375ad5a8168dd613284fbffe99d23f535cbb0db
Salah Haddani <salahabdelwahabe@example.com>
2026-01-28 19:01:39 +0100

    relocate the navbar from app component to home component

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -3,20 +3,34 @@ import { RecipeService } from '../../services/recipe.service';
 import { CommonModule } from '@angular/common';
 import { MatCardContent, MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, MatCardModule, RouterLink],
+  imports: [CommonModule, MatCardModule, RouterLink, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
   recipes: any[] = [];
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
   ngOnInit(): void {
     this.recipeService.getAllRecipes().subscribe({
       next: (data) => (this.recipes = data),
       error: (err) => console.error('could not load recipes', err),
     });
+  }
+  isPublisher(): boolean {
+    const token = localStorage.getItem('access_token');
+    if (!token) return false;
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return payload.role === 'publisher';
+  }
+  logout() {
+    this.authService.logout();
   }
 }
