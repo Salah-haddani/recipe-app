093dcc5d916da38013934cceff11a72c695739c4
Salah Haddani <salahabdelwahabe@example.com>
2026-01-28 18:30:26 +0100

    add recipe details component

---
+++ client/src/app/pages/recipe-details/recipe-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from '../../services/recipe.service';
+
+@Component({
+  selector: 'app-recipe-details',
+  imports: [],
+  templateUrl: './recipe-details.component.html',
+  styleUrl: './recipe-details.component.css',
+})
+export class RecipeDetailsComponent implements OnInit {
+  recipe: any;
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.recipeService
+        .getRecipeById(id)
+        .subscribe((data) => (this.recipe = data));
+    }
+  }
+}
