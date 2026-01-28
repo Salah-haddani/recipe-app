f7e6f728472824e3468ac467a23289ae074d6893
Salah Haddani <salahabdelwahabe@example.com>
2026-01-28 16:12:45 +0100

    edit home page to show all recipes

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../../services/recipe.service';
+import { CommonModule } from '@angular/common';
+import { MatCardContent, MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, MatCardModule, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  recipes: any[] = [];
+  constructor(private recipeService: RecipeService) {}
+  ngOnInit(): void {
+    this.recipeService.getAllRecipes().subscribe({
+      next: (data) => (this.recipes = data),
+      error: (err) => console.error('could not load recipes', err),
+    });
+  }
 }
