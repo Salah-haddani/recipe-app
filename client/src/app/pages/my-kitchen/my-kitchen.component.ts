7beb01a07fb1f94c680c61fbba95f7fba442f93b
Salah Haddani <salahabdelwahabe@example.com>
2026-01-29 15:56:29 +0100

    edit my-kitchen component

--- client/src/app/pages/my-kitchen/my-kitchen.component.ts
+++ client/src/app/pages/my-kitchen/my-kitchen.component.ts
@@ -3,16 +3,41 @@ import { RecipeService } from '../../services/recipe.service';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-my-kitchen',
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './my-kitchen.component.html',
   styleUrl: './my-kitchen.component.css',
 })
 export class MyKitchenComponent implements OnInit {
   myRecipes: any[] = [];
-  constructor(private recipeService: RecipeService) {}
+  recipeForm: FormGroup;
+  isEditing = false;
+  currentRecipeId: string | null = null;
+
+  constructor(private recipeService: RecipeService, private fb: FormBuilder) {
+    this.recipeForm = this.fb.group({
+      title: ['', Validators.required],
+      ingredients: ['', Validators.required], // Will split by ','
+      instructions: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
     this.loadMyRecipes();
@@ -25,6 +50,44 @@ export class MyKitchenComponent implements OnInit {
       this.myRecipes = recipes.filter((r) => r.owner._id === user_id); //filter recipes where owner is the user logged in
     });
   }
-  editRecipe(recipe: any) {}
-  deleteRecipe(id: string) {}
+  onSubmit() {
+    if (this.recipeForm.invalid) return;
+    const recipeData = {
+      ...this.recipeForm.value,
+      ingredients: this.recipeForm.value.ingredients
+        .split(',')
+        .map((i: string) => i.trim()),
+    };
+    if (this.isEditing && this.currentRecipeId) {
+      this.recipeService
+        .updateRecipe(this.currentRecipeId, recipeData)
+        .subscribe(() => {
+          this.resetForm();
+          this.loadMyRecipes();
+        });
+    } else {
+      this.recipeService.createRecipe(recipeData).subscribe(() => {
+        this.resetForm();
+        this.loadMyRecipes();
+      });
+    }
+  }
+
+  editRecipe(recipe: any) {
+    this.isEditing = true;
+    this.currentRecipeId = recipe._id;
+    this.recipeForm.patchValue({
+      title: recipe.title,
+      ingredients: recipe.ingredients.join(', '),
+      instructions: recipe.instructions,
+    });
+  }
+  deleteRecipe(id: string) {
+    this.recipeService.deleteRecipe(id).subscribe(() => this.loadMyRecipes());
+  }
+  resetForm() {
+    this.isEditing = false;
+    this.currentRecipeId = null;
+    this.recipeForm.reset();
+  }
 }
