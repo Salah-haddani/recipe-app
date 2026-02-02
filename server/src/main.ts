841968c8a0c1d3a83f10cef84c50f051fb72745c
Salah Haddani <salahabdelwahabe@example.com>
2026-02-02 09:56:37 +0100

    add headers for security - helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,7 @@ async function bootstrap() {
     origin: 'http://localhost:4200',
     Credentials: true,
   });
+  app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
