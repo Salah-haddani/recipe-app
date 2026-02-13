3eec8ed30e764fa672afaabe2bcb299fa56494de
Salah Haddani <salahabdelwahabe@example.com>
2026-02-13 12:21:48 +0100

    edit home page component - pagination

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -14,15 +14,38 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class HomeComponent implements OnInit {
   recipes: any[] = [];
+  totalRecipes = 0;
+  currentPage = 1;
+  pageSize = 6; // 6 recipes for each page
+  currentSort = 'desc';
   constructor(
     private recipeService: RecipeService,
     private authService: AuthService
   ) {}
   ngOnInit(): void {
-    this.recipeService.getAllRecipes().subscribe({
-      next: (data) => (this.recipes = data),
-      error: (err) => console.error('could not load recipes', err),
-    });
+    this.loadRecipes();
+  }
+  loadRecipes(): void {
+    this.recipeService
+      .getAllRecipes(this.currentPage, this.pageSize, this.currentSort)
+      .subscribe({
+        next: (data) => {
+          this.recipes = data.recipes;
+          this.totalRecipes = data.total;
+        },
+        error: (err) => console.error('could not load recipes', err),
+      });
+  }
+
+  changePage(newPage: number): void {
+    this.currentPage = newPage;
+    this.loadRecipes();
+  }
+
+  toggleSort(): void {
+    this.currentSort = this.currentSort === 'desc' ? 'asc' : 'desc';
+    this.currentPage = 1; // Reset to first page
+    this.loadRecipes();
   }
   isPublisher(): boolean {
     const token = localStorage.getItem('access_token');
